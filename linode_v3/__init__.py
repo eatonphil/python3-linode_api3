cb32a31771742dded2a3721ada29343ad7d5bf41
Phil Eaton <peaton@linode.com>
2017-03-05 10:44:29 -0500

    Add pypi package structures

---
+++ linode_v3/__init__.py
@@ -0,0 +1,1 @@
+from api import linode, avail
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='linode_v3',
+      version='1.0',
+      description='Linode APIv3 client for Python3',
+      author='Phil Eaton',
+      url='https://github.com/eatonphil/python-linode-apiv3',
+      packages=['linode_v3'])
