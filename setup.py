6d846d53235552fd51c32ca1de949482f3e66d28
Phil Eaton <peaton@linode.com>
2017-04-09 16:23:53 -0400

    Package for PyPi

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='linode_v3',
-      version='1.0',
+setup(name='linode-api3',
+      version='0.1',
       description='Linode APIv3 client for Python3',
       author='Phil Eaton',
-      url='https://github.com/eatonphil/python-linode-apiv3',
-      packages=['linode_v3'])
+      url='https://github.com/eatonphil/python3-linode-api3',
+      install_requires=['requests'],
+      packages=['linode-api3'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
